4191d7505af890af337d6f69336ad0a3532cf179
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-07-30 11:08:20 +0700

    feat: bump actions

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,15 @@
 // @ts-check
-
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+const site = process.env.SITE || "http://localhost:4321";
+const base = process.env.BASE || "/";
+
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? "https://area44.github.io" : "http://localhost:4321",
-  base: "astro-coverflow-carousel",
+  site,
+  base,
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
