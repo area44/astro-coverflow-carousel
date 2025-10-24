f82ba013d479593cdcda683c910c4154fc170106
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-10-24 14:34:28 +0700

    fix: site var

--- astro.config.ts
+++ astro.config.ts
@@ -3,8 +3,8 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 
-const site = process.env.SITE || "http://localhost:4321";
-const base = process.env.BASE || "/";
+const site = process.env.site || "http://localhost:4321";
+const base = process.env.base || "/";
 
 // https://astro.build/config
 export default defineConfig({
